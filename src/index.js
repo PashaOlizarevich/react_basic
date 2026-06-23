d65b5415b50371af111e1e6edd0d782d54262172
Pasha Olizarevich <pasha.sokol199@icloud.com>
2026-06-23 16:55:54 +0300

    lesson 122

--- src/index.js
+++ src/index.js
@@ -3,19 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const elem = (
-  <div>
-    <h2>Hello world!</h2>
-    <input type="text" />
-    <button>Click</button>
-  </div>
-);
+
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-elem,
+<App/>,
 );
 
 
